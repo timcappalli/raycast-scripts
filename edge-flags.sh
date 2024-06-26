6fecc0ab8ff88fdfd2b24aaec4d205daecd9b28f
Tim Cappalli <tim@cloudauth.dev>
2024-06-27 05:08:11 +0900

    Add WebAuthn JSON serialization flag

--- edge-flags.sh
+++ edge-flags.sh
@@ -10,10 +10,10 @@
 # @raycast.packageName me.timcappalli.raycast.edge.flags
 
 # @raycast.argument1 { "type": "dropdown", "placeholder": "Version", "data" : [{"title" : "Stable", "value": "edge"}, {"title" : "Beta", "value": "beta"}, {"title" : "Dev", "value": "dev"}, {"title" : "Canary", "value": "canary"}], "optional": false}
-# @raycast.argument2 { "type": "dropdown", "placeholder": "Flag", "data" : [{"title" : "All WebAuthn Flags", "value": "allwebauthn"}, {"title" : "WebAuthn Hints", "value": "WebAuthenticationHints"}, {"title" : "WebAuthn Related Origins", "value": "WebAuthenticationRelatedOrigin"}, {"title" : "WebAuthn LargeBlob Extension", "value": "WebAuthenticationLargeBlobExtension"}], "optional": false}
+# @raycast.argument2 { "type": "dropdown", "placeholder": "Flag", "data" : [{"title" : "All WebAuthn Flags", "value": "allwebauthn"}, {"title" : "WebAuthn Hints", "value": "WebAuthenticationHints"}, {"title" : "WebAuthn Related Origins", "value": "WebAuthenticationRelatedOrigin"}, {"title" : "WebAuthn LargeBlob Extension", "value": "WebAuthenticationLargeBlobExtension"}, {"title": "WebAuthn JSON Serialization", "value": "WebAuthenticationJSONSerialization"], "optional": false}
 
 if [ $2 = "allwebauthn" ]; then
-  flag="WebAuthenticationHints,WebAuthenticationRelatedOrigin,WebAuthenticationLargeBlobExtension"
+  flag="WebAuthenticationHints,WebAuthenticationRelatedOrigin,WebAuthenticationLargeBlobExtension,WebAuthenticationJSONSerialization"
 else
   flag=$2
 fi
