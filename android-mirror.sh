63e43b2cdcf38bc0c8ca7c38e3231ec26822cc14
Tim Cappalli <tim@cloudauth.dev>
2025-06-11 20:04:19 -0400

    update scrcopy with record options

--- android-mirror.sh
+++ android-mirror.sh
@@ -9,7 +9,9 @@
 # @raycast.icon icons/android-robot.svg
 # @raycast.packageName me.timcappalli.raycast.android.mirroring
 
-# @raycast.argument1 { "type": "dropdown", "placeholder": "Mode", "data" : [{"title" : "Mirror + Control", "value": "mc"}, {"title" : "Mirror + Audio", "value": "ma"},{"title" : "Mirror Only", "value": "m"}, {"title" : "Mirror + Control + Audio", "value": "mca"}], "optional": false}
+# @raycast.argument1 { "type": "dropdown", "placeholder": "Mode", "data" : [{"title" : "Mirror + Control", "value": "mc"}, {"title" : "Mirror + Audio", "value": "ma"},{"title" : "Mirror Only", "value": "m"}, {"title" : "Mirror + Control + Audio", "value": "mca"}, {"title" : "Record: Video Only", "value": "rv"}, {"title" : "Record", "value": "v"}], "optional": false}
+
+cd ~/zWorkFolder/
 
 if [ $1 = "mc" ]; then
   scrcpy --video-codec=h265 --max-size=1920 --max-fps=60 --no-audio --keyboard=uhid
@@ -19,6 +21,10 @@ elif [ $1 = "mca" ]; then
   scrcpy --video-codec=h265 --max-size=1920 --max-fps=60 --keyboard=uhid
 elif [ $1 = "ma" ]; then
   scrcpy --video-codec=h265 --max-size=1920 --max-fps=60
+elif [ $1 = "rv" ]; then
+  scrcpy --no-audio --record=android-recording.mp4 --record-format=mp4 --max-size=1920 --max-fps=60 --show-touches
+elif [ $1 = "r" ]; then
+    scrcpy --no-audio --record=android-recording.mp4 --record-format=mp4 --max-size=1920 --max-fps=60 --show-touches
 else
   echo "No valid combos"
 fi
